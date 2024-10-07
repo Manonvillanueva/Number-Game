d390645c586e11aa26e610d1f0ae3da7668caf4c
ManonVillanueva <g.manon14@gmail.com>
2024-10-07 11:45:15 +0200

    add comments

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const btns = document.querySelectorAll(".btn");
 const validBtn = document.querySelector(".validBtn");
+
+// Génère un nombre aléatoire entre 0 et 100
 const randomNumb = Math.floor(Math.random() * 100);
+
+// Initialise un tableau vide qui va stocker les différentes tentatives
 let attempts = [];
+
 console.log(randomNumb);
 
+// Lorsque qu'un btn (chiffre) est selectionné, son ID (son ID correspond au chiffre affiché dans chaque btn) est récupéré et l'ajoute au champ d'affichage, on utilise += pour concaténer ces chiffres
 btns.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     displayNumber.value += e.target.id;
@@ -11,9 +17,13 @@ btns.forEach((btn) => {
 });
 
 validBtn.addEventListener("click", () => {
-  console.log(attempts);
-
+  // Récupérer la valeur de l'utilisateur en recupérant la valeur de l'input displayNumber
   const userValue = displayNumber.value;
+
+  // Stocker le message correspondant à chaque tentative
+  let resultMessage;
+
+  // Comparaison entre la saisie de l'utilisateur avec le nombre aléatoire généré
   if (userValue == randomNumb) {
     resultMessage = `Tentative ${
       attempts.length + 1
@@ -27,13 +37,19 @@ validBtn.addEventListener("click", () => {
       attempts.length + 1
     } : ${userValue} - C'est un peu léger !`;
   }
+  // Ajoute le message correspondant à cette tentative dans le tableau "attempts"
   attempts.push(resultMessage);
+
+  // Fonction qui met à jour l'affichage des tentatives
   displayAttempts();
+
+  // Vide le contenu l'input avant d'ajouter de nouvelles tentatives
   displayNumber.value = "";
 });
 
+// Mis à jour de l'affichage des tentatives
 function displayAttempts() {
-  resultGame.innerHTML = "";
+  // resultGame.innerHTML = "";
   attempts.forEach((attempt) => {
     const p = document.createElement("p");
     p.textContent = attempt;
